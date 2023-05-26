c8282f6f8a8bad0b95fe3c20c4bcdd1fcf40b0c2
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-05-26 01:49:47 -0700

    About us component fixed

--- src/Pages/AboutWithires.js
+++ src/Pages/AboutWithires.js
@@ -1,49 +1,49 @@
-import React from 'react'
+import React from 'react';
 import MainLayout from '../components/Layouts/MainLayout';
-import about from '../assets/about.jpg'
-import { Link } from 'react-router-dom';
-import Vision from '../components/About/Vision';
+import about from '../assets/about.jpg';
 
 const AboutWithires = () => {
   return (
     <MainLayout>
-    <div className="container mx-auto py-10 px-4 sm:px-6 lg:py-20 lg:px-8">
-      {/* <h2 className="text-center text-4xl font-bold mb-10">Our Instructors Work at</h2>
-      <div className="flex flex-wrap justify-center">
-        <div className="overflow-hidden mx-2 my-2 sm:mx-4 sm:my-2 w-1/2 sm:w-1/4">
-          <img src={} alt="Instructor 1" className="w-full object-cover" />
+      <div className="flex flex-col md:flex-row-reverse rounded-lg overflow-hidden mt-12">
+        <div className="md:w-1/2 px-6 py-4 md:px-10 md:py-6">
+          <h1 className="text-3xl font-bold mb-4">About us</h1>
+          <p className="text-gray-700 text-base text-white font-medium">
+            Wit Hires Consulting is a technology-driven deep jobs platform that gives clients
+            the fastest access on the market to the support they need with highly skilled specialists
+            and industry-leading tech professionals. We help source, hire, onboard, and manage the talent
+            you need at the push of a button. Our 'remote-first' culture allows us to support clients on a
+            global scale and to source our own talent to match the requirements, time zones, and languages needed.
+            Whether you need one person or an entire team, we match you with highly skilled talents in just 24 hours.
+          </p>
+          <button className="px-4 py-2 rounded-lg bg-gray-800 font-semibold text-white mt-4">Enroll now</button>
         </div>
-        <div className="overflow-hidden mx-2 my-2 sm:mx-4 sm:my-2 w-1/2 sm:w-1/4">
-          <img src={} alt="Instructor 2" className="w-full object-cover" />
+        <div className="md:w-1/2 pl-8">
+          <img src={about} alt="About us" className="w-full h-auto object-cover rounded-lg animate-fade-in-down" />
         </div>
-        <div className="overflow-hidden mx-2 my-2 sm:mx-4 sm:my-2 w-1/2 sm:w-1/4">
-          <img src={} alt="Instructor 3" className="w-full object-cover" />
-        </div> */}
-        <div className="flex flex-col md:flex-row rounded-lg overflow-hidden mt-12 animate-fade-in-up">
-            <div className="md:w-2/3 h-screen">
-              <img src={about} alt="place" />
-            </div>
-            <div className="p-6 md:w-2/3">
-              <h2 className="text-2xl font-bold mb-2">About us</h2>
-              <p className="text-gray-800 text-base text-white font-medium">
-              WitHires Consulting is a technology-driven deep jobs platform that give clients the
-              fastest access on the market to the support they need with highly skilled specialists
-              and industry leading tech professionals. We help source, hire, onboard and manage the
-              talent you need at the push of a button. Our ‘remote-first’ culture allows us to support
-              clients on a global scale, and to source our own talent to match the requirements, time 
-              zones and languages needed. Whether you need one person or an entire team, we match you with
-              highly skilled talents in just 24 hours.
-              </p>
-              <Link to='/hireprofessionals'>
-                  <button className='px-2 py-2 bg-white rounded-lg font-semibold text-gray-800 mt-2 '>Talent Pool </button>
-              </Link>
-            </div>
-        </div>
-        <Vision />
       </div>
-    {/* </div> */}
-    </MainLayout>
-  )
-}
 
-export default AboutWithires
+      <div className="flex flex-col md:flex-row-reverse rounded-lg overflow-hidden mt-12">
+        <div className="md:w-1/2">
+          <img src="https://media.istockphoto.com/id/1264334407/photo/coworkers-doing-a-meeting-outdoors.jpg?s=612x612&w=0&k=20&c=rCIRjU-Wns_1X4nyFOYBfouP6h595Do6aFDoq0FYGWE=" alt="Vision" className="rounded-lg object-cover animate-fade-in-down" />
+        </div>
+        <div className="md:w-1/2 p-6 pl-8">
+          <h1 className="text-3xl font-bold mb-4">Our Vision</h1>
+          <p className="text-gray-700 text-base text-white font-medium">
+            At Withires, our vision is to be the leading provider of exceptional services,
+            renowned for our unwavering commitment to quality, integrity, support, and continuous innovation.
+            We aspire to be recognized as the foremost choice for clients seeking unparalleled excellence, driven
+            by our relentless pursuit of hiring the top 1% of professionals who consistently deliver impactful
+            solutions. By maintaining an exceptional level of quality, we ensure that our clients' needs are not
+            only met but exceeded. Integrity is the bedrock of our organization. We consistently do the right
+            thing, even when no one is watching. Our dedication to honesty, trust, and transparency inspires
+            admiration from both clients and colleagues alike, fostering enduring relationships built on mutual
+            respect.
+          </p>
+        </div>
+      </div>
+    </MainLayout>
+  );
+};
+
+export default AboutWithires;
