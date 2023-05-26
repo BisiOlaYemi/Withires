a7b5ba0ea13d4535a11c2f1d43cdb32a97357345
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-05-26 00:59:37 -0700

    Adjusting the about us component

--- src/components/About/Vision.jsx
+++ src/components/About/Vision.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Vision = () => {
   return (
-    <div className="flex flex-col md:flex-row-reverse rounded-lg overflow-hidden mt-24">
+    <div className="flex flex-col md:flex-row-reverse rounded-lg overflow-hidden">
         <div className="md:w-1/2">
           <img src="https://media.istockphoto.com/id/1264334407/photo/coworkers-doing-a-meeting-outdoors.jpg?s=612x612&w=0&k=20&c=rCIRjU-Wns_1X4nyFOYBfouP6h595Do6aFDoq0FYGWE=" 
           alt="Vision" className=" rounded-lg object-cover animate-fade-in-down" />
         </div>
         <div className="md:w-1/2 p-6 pl-8">
           <h1 className="text-3xl font-bold mb-4">Our Vision</h1>
-          <p className="text-gray-700 text-base text-primary font-[Verdana]">
+          <p className="text-gray-700 text-base text-white font-medium">
           At Withires, our vision is to be the leading provider of exceptional services,
           renowned for our unwavering commitment to quality, integrity, support, and continuous innovation.
           We aspire to be recognized as the foremost choice for clients seeking unparalleled excellence, driven
