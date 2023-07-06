6a255896f353be66a7e3df37501814eb7324494b
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-07-06 03:30:21 -0700

    Reversed change to previous

--- src/Pages/Courses.js
+++ src/Pages/Courses.js
@@ -20,12 +20,12 @@ const Courses = () => {
     <MainLayout>
       <div className="container mx-auto py-8 mt-12">
         <h2 className="text-3xl font-bold text-center mb-6">Courses</h2>
-        <p className="text-white text-center font-semibold mb-4">"Get the needed Training in Tech"</p>
         <div className="flex flex-wrap justify-center gap-4 animate-fade-in-down">
           {technos.map((techno, index) => (
             <div key={index} className="bg-gray-800 py-24 rounded-lg shadow-md overflow-hidden">
               <div className="p-6">
                 <h3 className="text-lg font-bold text-white mb-2">{techno.course}</h3>
+                <p className="text-white">"Get the needed Training in Tech"</p>
               </div>
               <div className=" py-3 px-4 text-right">
                 <button
