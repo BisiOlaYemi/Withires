7db0bcfd492a7ef8eb862ebc2b3f63a9718fde66
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-07-16 02:30:07 -0700

    Updated the link in react router

--- src/components/Homepage/Hero.jsx
+++ src/components/Homepage/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import bgrm from '../../assets/bgrm.png';
 import { Link } from 'react-router-dom';
-// import { Link } from 'react-router-dom';
+
 
 const Hero = () => {
   return (
