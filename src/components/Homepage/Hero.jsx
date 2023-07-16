d6428b9df778cba60fd6b952930e0953b83e61ba
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-07-16 02:19:24 -0700

    Added span to dream job

--- src/components/Homepage/Hero.jsx
+++ src/components/Homepage/Hero.jsx
@@ -1,25 +1,43 @@
 import React from 'react';
 import bgrm from '../../assets/bgrm.png';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const Hero = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center animate-fade-in-down">
-      {/* Content */}
-      <div className="flex flex-col justify-center max-w-md px-16 py-8 md:py-16 md:w-1/3 xl:w-1/4">
-        <h1 className="text-4xl font-bold mb-4 text-gray-800">Dream Jobs.Top Talent. All in One Place </h1>
-        <p className="text-lg mb-6 text-white font-medium">Our ‘remote-first’ culture allows us to support clients on a global scale</p>
-        <Link to='/hireprofessionals'>
-        <button className="px-4 py-2 bg-gray-800 text-white font-bold rounded-full hover:bg-blue-600">
-          Talent Pool
-        </button>
-        </Link>
-      </div>
-      {/* Image Card */}
-      <div className="flex-shrink-0 w-full md:w-2/3 xl:w-3/4">
-        <div className="h-64 md:h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${bgrm})` }}></div>
-      </div>
-    </div>
+    <section
+            className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full sm:m12"
+            id="home"
+        >
+            <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6 mt-24 p-8">
+
+
+                <p className="text-[2.5rem] text-center md:text-left lg:text-[3.5rem] font-bold tracking-wide text-headingColor md:w-[85%] md:leading-tight">
+                    <span className="bg-gradient-to-r from-purple-500 to-amber-500 text-transparent bg-clip-text text-[3rem] lg:text-[4rem]">Dream Jobs.</span> Top Talent. All in One Place 
+                </p>
+
+                <p className="text-center md:text-justify text-lighttextGray text-sm md:w-[85%] flex flex-wrap gap-1">
+                Our ‘remote-first’ culture allows us to support clients on a global scale
+                </p>
+
+
+                <div className="text-center md:w-[85%] flex flex-wrap gap-1 md:gap-3">
+                    <input type="text" className="md:w-3/4 border px-2 py-3 rounded-lg " placeholder="Search Jobs"/>
+                    <button className="bg-lightPrimary text-lightCard md:px-7 px-4 py-3   text-sm rounded-lg">Search</button>
+                </div>
+
+
+            </div>
+            <div className="py-2 flex-1 flex items-center relative">
+                <img
+                    src={bgrm}
+                    className=" ml-auto h-420 w-full lg:w-auto lg:h-650"
+                    alt="hero-bg"
+                />
+            </div>
+
+
+        
+        </section>
   );
 };
 
