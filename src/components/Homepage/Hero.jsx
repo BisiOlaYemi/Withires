aa1bb66fff758f18ea557332af2fe8e6aac36984
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-07-16 02:31:57 -0700

    Adjusted the component to fit

--- src/components/Homepage/Hero.jsx
+++ src/components/Homepage/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
             className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full sm:m12"
             id="home"
         >
-            <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6 mt-24 p-8">
+            <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6 mt-12 p-8">
 
 
                 <p className="text-[2.5rem] text-center md:text-left lg:text-[3.5rem] font-bold tracking-wide text-headingColor md:w-[85%] md:leading-tight">
