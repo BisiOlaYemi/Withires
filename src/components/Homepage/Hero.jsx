4dcc7a50e735fe8fbca138ff9573f77274bee556
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-07-19 01:57:09 -0700

    Added animation to hero bg

--- src/components/Homepage/Hero.jsx
+++ src/components/Homepage/Hero.jsx
@@ -33,7 +33,7 @@ const Hero = () => {
             <div className="py-2 flex-1 flex items-center relative">
                 <img
                     src={bgrm}
-                    className=" ml-auto h-420 w-full lg:w-auto lg:h-650"
+                    className=" ml-auto h-420 w-full lg:w-auto lg:h-650 animate-fade-in-down"
                     alt="hero-bg"
                 />
             </div>
